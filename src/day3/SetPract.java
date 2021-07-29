f69c99b7a20826ed0a8e8b096f904d2b417a2230
Jikaoro <j.orozbaeva@gmail.com>
2021-07-29 13:47:06 -0500

    set is cool cant wait to learn

--- src/day3/SetPract.java
+++ src/day3/SetPract.java
@@ -4,4 +4,5 @@ public class SetPract {
     // newcomment
     // set is a collection type that store unique item
     // if you add duplicate it will just remove it
+    // it's a collection of unique elements
 }
